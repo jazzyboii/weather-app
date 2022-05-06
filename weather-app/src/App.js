915d4bc3b4e4d0383bc18f4c5cb4da606a428ab9
David Vincent <70216205+jazzyboii@users.noreply.github.com>
2022-05-06 15:27:13 -0400

    weather app pg1

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Box,Card,CardActions,CardContent,Button,TextField,Typography,Grid,} from '@mui/material';
+import "./App.css";
+//import { styled } from '@mui/material/styles';
+//style={{justifyContent: 'center'}}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return (
+      <>
+      </>
+    );
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App1() {
+    return (
+      <>
+       <Grid container 
+        justify="center" 
+        alignItems="center" 
+        direction="column" 
+        spacing={5}
+        style={{marginBottom: "1em"}}
+      >
+        <Grid item>
+          <Typography variant="h4" color="primary" style={{marginTop: "1em"}} >
+            Weather app
+          </Typography>
+        </Grid>
+        <Grid item >
+          <Login/>
+        </Grid>          
+       </Grid>
+       <Box textAlign='center'>
+        <Button variant='contained'>
+          Submit
+        </Button>
+       </Box>           
+      </>
+    );
+}
+
+const Login = () => {
+  return (
+    <>
+      <Card sx={{ minWidth: 275 }} style={{marginBottom: "2em"}} variant="outlined">
+        <CardContent>
+          <TextField variant="outlined" label="City" fullWidth style={{marginBottom: "2em"}}/>  
+           <TextField variant="outlined" label="State" fullWidth/>
+        </CardContent>
+        <CardContent>
+          <TextField variant="outlined" label="Country" fullWidth/>        
+        </CardContent>
+        <CardActions >
+          <Button size="medium">Current Location</Button>
+        </CardActions>
+      </Card> 
+      <Card sx={{ minWidth: 275 }} variant="outlined">
+      <CardContent>
+        <TextField variant="outlined" label="Zipcode" fullWidth/>
+      </CardContent>
+      </Card>    
+    </>
+       
+  );  
+}
+
+export default App1;
+
+
+
